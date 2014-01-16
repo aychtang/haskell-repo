0ad56f618f7fcf029bbf6dfc04e92bb1958c2bf8
Howard Tang <tangh@google.com>
2014-01-16 11:46:37 +0000

    Adds factors function.

---
+++ src/ld.hs
@@ -0,0 +1,19 @@
+divisible :: Integer -> Integer -> Bool
+divisible d n = rem n d == 0
+
+ldf :: Integer -> Integer -> Integer
+ldf k n
+	| divisible k n = k
+  | k^2 > n       = n
+  | otherwise     = ldf (k + 1) n
+
+ld :: Integer -> Integer
+ld n = ldf 2 n
+
+factors :: Integer -> [Integer]
+factors n
+	| n == 1    = []
+	| otherwise = p : factors (div n p)
+	  where p 	= ld n
+
+main = print (factors (84))
